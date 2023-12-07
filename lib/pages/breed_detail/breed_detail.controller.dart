70f5638572501a148ef4ba040005aea613bce177
Luca Oropallo <luca.oropallo.pc@gmail.com>
2023-12-07 17:55:04 +0100

    feat(detail): add small header animation

--- lib/pages/breed_detail/breed_detail.controller.dart
+++ lib/pages/breed_detail/breed_detail.controller.dart
@@ -1,12 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../design/tokens/dimensions.token.dart';
 import '../../store/breed/breed.model.dart';
 import '../../store/breed/breed.service.dart';
 
 class BreedDetailPageController extends GetxController {
+  final scrollController = ScrollController();
+  final smallHeaderOpacity = .0.obs;
+
   BreedModel? get breed => BreedService.to.breeds.firstWhereOrNull(
         (element) => element.name == Get.parameters['breed_name'],
       );
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    scrollController.addListener(() {
+      final height = Get.width / CustomRatioDimension.tall.value -
+          Get.mediaQuery.padding.top -
+          90;
+      smallHeaderOpacity.value =
+          clampDouble((scrollController.offset - height) / 90, 0, 1);
+    });
+  }
 
   @override
   void onReady() {
--- lib/pages/breed_detail/breed_detail.widget.dart
+++ lib/pages/breed_detail/breed_detail.widget.dart
@@ -21,70 +21,126 @@ class BreedDetailPageWidget extends GetView<BreedDetailPageController> {
 
     return SafeArea(
       top: false,
-      child: Obx(
-        () => ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            _ImageHeader(breed: controller.breed),
-            if (controller.breed?.subBreeds.isNotEmpty == true) ...[
-              SizedBox(height: CustomSpaceDimension.xl.value),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: CustomSpaceDimension.lg.value,
-                ),
-                child: Text(
-                  'Sub breeds',
-                  style: CustomText.h2.style,
-                ),
-              ),
-              SizedBox(height: CustomSpaceDimension.md.value),
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  horizontal: CustomSpaceDimension.lg.value,
-                ),
-                child: Wrap(
-                  spacing: CustomSpaceDimension.md.value,
-                  children: controller.breed!.subBreeds
-                      .map((e) => Chip(label: Text(e)))
-                      .toList(),
-                ),
-              )
-            ],
-            SizedBox(height: CustomSpaceDimension.xl.value),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: CustomSpaceDimension.lg.value),
-              child: Text(
-                'Gallery',
-                style: CustomText.h2.style,
-              ),
-            ),
-            if (controller.breed != null)
-              ...controller.breed!.images.map(
-                (element) => Padding(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: CustomSpaceDimension.lg.value,
-                    vertical: CustomSpaceDimension.md.value,
+      child: Stack(
+        children: [
+          Obx(
+            () => ListView(
+              controller: controller.scrollController,
+              padding: EdgeInsets.zero,
+              children: [
+                _ImageHeader(breed: controller.breed),
+                if (controller.breed?.subBreeds.isNotEmpty == true) ...[
+                  SizedBox(height: CustomSpaceDimension.xl.value),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: CustomSpaceDimension.lg.value,
+                    ),
+                    child: Text(
+                      'Sub breeds',
+                      style: CustomText.h2.style,
+                    ),
                   ),
-                  child: CustomCard(
-                    child: Image(
-                      width: width,
-                      height: height,
-                      fit: BoxFit.cover,
-                      errorBuilder: (_, __, ___) => ErrorImage(height: height),
-                      image: ResizeImage(
-                        NetworkImage(element),
-                        width: width.toInt(),
+                  SizedBox(height: CustomSpaceDimension.md.value),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: CustomSpaceDimension.lg.value,
+                    ),
+                    child: Wrap(
+                      spacing: CustomSpaceDimension.md.value,
+                      children: controller.breed!.subBreeds
+                          .map((e) => Chip(label: Text(e)))
+                          .toList(),
+                    ),
+                  )
+                ],
+                SizedBox(height: CustomSpaceDimension.xl.value),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                      horizontal: CustomSpaceDimension.lg.value),
+                  child: Text(
+                    'Gallery',
+                    style: CustomText.h2.style,
+                  ),
+                ),
+                if (controller.breed != null)
+                  ...controller.breed!.images.map(
+                    (element) => Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: CustomSpaceDimension.lg.value,
+                        vertical: CustomSpaceDimension.md.value,
+                      ),
+                      child: CustomCard(
+                        child: Image(
+                          width: width,
+                          height: height,
+                          fit: BoxFit.cover,
+                          errorBuilder: (_, __, ___) =>
+                              ErrorImage(height: height),
+                          image: ResizeImage(
+                            NetworkImage(element),
+                            width: width.toInt(),
+                          ),
+                        ),
                       ),
                     ),
                   ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Obx(
+              () => IgnorePointer(
+                ignoring: controller.smallHeaderOpacity.value < 1,
+                child: Opacity(
+                  opacity: controller.smallHeaderOpacity.value,
+                  child: _SmallHeader(breed: controller.breed),
                 ),
-              )
-          ],
-        ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class _SmallHeader extends StatelessWidget {
+  const _SmallHeader({required this.breed});
+
+  final BreedModel? breed;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          border: Border(bottom: BorderSide(color: Colors.grey)),
+        ),
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).padding.top +
+              CustomSpaceDimension.lg.value,
+          bottom: CustomSpaceDimension.lg.value,
+          right: CustomSpaceDimension.lg.value,
+          left: CustomSpaceDimension.lg.value,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              child: Text(
+                breed?.name?.capitalize ?? '',
+                style: CustomText.h1.style,
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.close),
+              color: Colors.black,
+              onPressed: Get.back,
+            ),
+          ],
+        ),
+      );
 }
 
 class _ImageHeader extends StatelessWidget {
@@ -118,6 +174,16 @@ class _ImageHeader extends StatelessWidget {
           child: Text(
             breed!.name?.capitalize ?? '',
             style: CustomText.h1.style.copyWith(color: Colors.white),
+          ),
+        ),
+        Positioned(
+          right: CustomSpaceDimension.lg.value,
+          top: CustomSpaceDimension.lg.value +
+              MediaQuery.of(context).padding.top,
+          child: IconButton(
+            icon: const Icon(Icons.close),
+            color: Colors.white,
+            onPressed: Get.back,
           ),
         ),
       ],
