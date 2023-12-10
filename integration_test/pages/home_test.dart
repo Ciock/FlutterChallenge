d64de5f924b3322846a7cf27d815e92182701c8b
Luca Oropallo <luca.oropallo.pc@gmail.com>
2023-12-10 17:25:49 +0100

    test(home): e2e test for searchBar interaction

--- integration_test/pages/home_test.dart
+++ integration_test/pages/home_test.dart
@@ -11,19 +11,15 @@ import 'package:integration_test/integration_test.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  group('end-to-end test', () {
-    testWidgets('Load breeds and open first detail page', (tester) async {
-      initServices();
-      // Load app widget.
+  group('end-to-end test Home Page', () {
+    setUpAll(initServices);
+
+    testWidgets('Load breeds, Interact with SearchBar, Open Breed Detail Page',
+        (tester) async {
       await tester.pumpWidget(const MainApp());
 
-      // Verify the Title
-      expect(find.text('Breeds'), findsOneWidget);
-
-      expect(find.text('No breed found'), findsOneWidget);
-
-      expect(find.byType(CustomSearchBar), findsOneWidget);
-
+      expect(find.byType(WelcomeAnimation), findsOneWidget);
+      // Await for WelcomeAnimation to end
       await Future.wait([
         BreedService.to.breeds$.first,
         Future.delayed(WelcomeAnimation.animationMinDuration),
@@ -31,12 +27,30 @@ void main() {
 
       await tester.pump();
 
+      // Verify the Title
+      expect(find.text('Breeds'), findsOneWidget);
+
+      // Test search bar interaction.
+      final searchBar = find.byType(CustomSearchBar);
+      expect(searchBar, findsOneWidget);
+
+      // enter random text to avoid matching any breed
+      await tester.enterText(searchBar, 'woenpwenckwxowbe');
+      await tester.pump();
+
+      // should show no BreedCard
+      expect(find.text('No breed found'), findsOneWidget);
+      expect(find.byType(BreedCard), findsNothing);
+
+      // clear search bar
+      await tester.enterText(searchBar, '');
+      await tester.pump();
+
+      expect(find.text('No breed found'), findsNothing);
       // Finds the BreedCard to tap on.
       final breedCard = find.byType(BreedCard);
 
       await tester.tap(breedCard.first);
-
-      // Trigger a frame.
       await tester.pumpAndSettle();
 
       // Verify the new current page is detail page.
